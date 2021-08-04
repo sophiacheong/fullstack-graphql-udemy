00f5485c9d48af38cdffe20474d09f44bb3b4a08
sophiacheong <sophiaacheong@gmail.com>
2021-08-03 19:53:09 -0700

    created boiler plate for mutation file

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -8,7 +8,16 @@ const UserType = require('./types/user_type');
 const mutation = new GraphQLObject({
   name: 'Mutation',
   fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
 
+      }
+    }
   }
 });
 
