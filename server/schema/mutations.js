11a41c06dfb7e16171adea2277c1820cb71050e8
sophiacheong <sophiaacheong@gmail.com>
2021-08-03 19:49:28 -0700

    made a mutation file for schema

---
+++ server/schema/mutations.js
@@ -0,0 +1,15 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+const UserType = require('./types/user_type');
+
+const mutation = new GraphQLObject({
+  name: 'Mutation',
+  fields: {
+
+  }
+});
+
+module.exports = mutation;
