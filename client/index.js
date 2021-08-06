a1770a5569cb67b17864a87f65788b69b461be46
sophiacheong <sophiaacheong@gmail.com>
2021-08-06 15:59:52 -0700

    added network interface

--- client/index.js
+++ client/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
