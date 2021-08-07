28e2e6da43939c1bb36bc820e85af3590510f39a
sophiacheong <sophiaacheong@gmail.com>
2021-08-06 17:22:26 -0700

    added log out mutation for logout button

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({});
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
 
     if (this.props.data.loading) { return <div />; }
 
     if (user) {
-      return
-      <div>
-        <li>
-          <Link to="/logout">Log out</Link>
-        </li>
-      </div>;
+      return (
+      <li>
+        <a onClick={this.onLogoutClick.bind(this)}>
+          Logout
+        </a>
+      </li> )
     } else {
       return (
         <div>
@@ -46,5 +51,7 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
 
