997ca34c2346850aa5a2cc4fea9fab7a9e80a851
sophiacheong <sophiaacheong@gmail.com>
2021-08-06 20:14:10 -0700

    hooked up onsubmit form for sign up form with auth form

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
+import AuthForm from './AuthForm';
 
 class SignUpForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    })
+  }
+
   render() {
     return (
-      <div>Sign Up</div>
+      <div>
+        <h3>Sign Up</h3>
+        <AuthForm
+          onSubmit={this.onSubmit.bind(this)}
+          errors={[]}
+        />
+      </div>
     )
   }
 }
 
-export default SignUpForm;
+export default graphql(mutation)(SignUpForm);
